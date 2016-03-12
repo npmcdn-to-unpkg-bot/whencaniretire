a1119324ce26af8730ca4cffbfbe1c46b7ad4454
David Gee <git@allpowerfuldave.com>
2016-03-11 23:21:43 -0600

    Started using bootstrap and Sass for CSS. Sass build config added to gulpfile.

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,30 +5,46 @@ import * as path from "path";
 import * as ts from "gulp-typescript";
 import * as nodemon from "gulp-nodemon";
 import * as plumber from "gulp-plumber";
+import * as sass from "gulp-sass";
 
 @Gulpclass()
 export class Gulpfile {
 
 
-  static paths = {
-    server: [
-      path.resolve("./server/src/**/*.ts"),
-      path.resolve("./server/typings/main.d.ts")
-    ],
-    client: [
-      path.resolve("./client/src/**/*.ts"),
-      path.resolve("./client/typings/browser.d.ts")
-    ],
-    html: [
-      path.resolve("./server/views/**/*.jade")
-    ]
+  static config = {
+    server: {
+      src: [
+        path.resolve("./server/src/**/*.ts"),
+        path.resolve("./server/typings/main.d.ts")
+      ],
+      dest: "",
+      watchTasks: ["buildServer"]
+    }
+    client: {
+      src: [
+        path.resolve("./client/src/**/*.ts"),
+        path.resolve("./client/typings/browser.d.ts")
+      ],
+      dest: "assets/js/client",
+      watchTasks: ["buildClient"]
+    },
+    html: {
+      src: path.resolve("./server/views/**/*.jade"),
+      dest: "views",
+      watchTasks: ["buildHtml"]
+    },
+    css: {
+      src: path.resolve("./assets/css/**/*.scss"),
+      dest: "assets/css",
+      watchTasks: ["buildCss"]
+    }
   };
 
   @Task("run", ["app", "watch"])
   run(){
   }
 
-  @Task("app", ["buildServer", "buildClient", "buildHtml"])
+  @Task("app", ["buildServer", "buildHtml", "buildClient", "buildCss"])
   app(){
     let nodemonSettings = {
       script: path.resolve("./dist/app.js"),
@@ -43,7 +59,7 @@ export class Gulpfile {
 
   @SequenceTask("runClean")
   runClean(){
-    return ["clean", "run"]];
+    return ["clean", "run"];
   }
 
   @Task("clean")
@@ -51,29 +67,34 @@ export class Gulpfile {
     return del(path.resolve("./dist/**"));
   }
 
-  @Task("build", ["buildServer", "buildHtml", "buildClient"])
+  @Task("build", ["buildServer", "buildHtml", "buildClient", "buildCss"])
   build(){
     return;
   }
 
-  @Task("watch", ["watchServer", "watchHtml", "watchClient"])
+  @Task("watch", ["watchServer", "watchHtml", "watchClient", "watchCss"])
   watch(){
     return;
   }
 
   @Task("watchServer")
   watchServer(){
-    return gulp.watch(Gulpfile.paths.server, ["buildServer"]);
+    return gulp.watch(Gulpfile.config.server.src, Gulpfile.config.server.watchTasks);
   }
 
   @Task("watchClient")
   watchClient(){
-    return gulp.watch(Gulpfile.paths.client, ["buildClient"]);
+    return gulp.watch(Gulpfile.config.client.src, Gulpfile.config.client.watchTasks);
   }
 
   @Task("watchHtml")
   watchHtml(){
-    return gulp.watch(Gulpfile.paths.html, ["buildHtml"]);
+    return gulp.watch(Gulpfile.config.html.src, Gulpfile.config.html.watchTasks);
+  }
+
+  @Task("watchCss")
+  watchCss(){
+    return gulp.watch(Gulpfile.config.css.src, Gulpfile.config.css.watchTasks);
   }
 
   @Task("buildServer")
@@ -81,10 +102,10 @@ export class Gulpfile {
     let tsProject = ts.createProject(path.resolve("./server/tsconfig.json"));
 
     return gulp
-      .src(Gulpfile.paths.server)
+      .src(Gulpfile.config.server.src)
       .pipe(ts(tsProject))
       .js
-      .pipe(gulp.dest(path.resolve("./dist")));
+      .pipe(gulp.dest(path.resolve("./dist", Gulpfile.config.server.dest)));
   }
 
   @Task("buildClient")
@@ -92,16 +113,25 @@ export class Gulpfile {
     let tsProject = ts.createProject(path.resolve("./client/tsconfig.json"));
 
     return gulp
-      .src(Gulpfile.paths.client)
+      .src(Gulpfile.config.client.src)
       .pipe(ts(tsProject))
       .js
-      .pipe(gulp.dest(path.resolve("./dist/assets/js/client")));
+      .pipe(gulp.dest(path.resolve("./dist", Gulpfile.config.client.dest)));
   }
 
   @Task("buildHtml")
   buildHtml(){
-    return gulp.src(Gulpfile.paths.html)
-      .pipe(gulp.dest(path.resolve("./dist/views")));
+    return gulp
+      .src(Gulpfile.config.html.src)
+      .pipe(gulp.dest(path.resolve("./dist", Gulpfile.config.html.dest)));
+  }
+
+  @Task("buildCss")
+  buildCss(){
+    return gulp
+      .src(Gulpfile.config.css.src)
+      .pipe(sass())
+      .pipe(gulp.dest(path.resolve("./dist", Gulpfile.config.css.dest)));
   }
 
   @SequenceTask()
