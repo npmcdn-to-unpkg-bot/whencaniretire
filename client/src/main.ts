5d214ef4807a8f42cd9df49abb85098285d1c130
David Gee <git@allpowerfuldave.com>
2016-03-11 22:25:59 -0600

    Working version of angular2 app

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,18 +1,12 @@
 System.config({
-  paths: {
-    "app/*": "assets/js/client/app/*.js",
-    "*": "*.js"
-  }
-  /*packages: {
-    "assets/js/client/app/main": {
-      format: "register",
-      defaultExtension: "js"
-    },
-    "assets/js/client/app/app.component": {
-      format: "register",
-      defaultExtension: "js"
+  "packages": {
+    "app": {
+      "defaultExtension": "js"
     }
-  }*/
+  },
+  "map": {
+    "app": "assets/js/client/app"
+  }
 });
 
 System.import("app/main").then(null, console.error.bind(console));
