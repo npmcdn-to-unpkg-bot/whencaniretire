3439f2356969f402b92979679446c131c54d8b2e
David Gee <git@allpowerfuldave.com>
2016-03-11 20:54:47 -0600

    Added 404 route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,19 +1,13 @@
 import {Request, Response} from "express";
 
 export function index(req: Request, res: Response, next: Function){
-  /*res.render("index", (err, html) => {
-    if(err){
-      console.log("in hander");
-      if(-1 !== err.message.indexOf("Failed to lookup view")){
-        return res.status(404).send("Not Found");
-      }
-      throw err;
-    }
-    res.send(html);
-  });*/
- res.render("index");
+  res.render("index");
 };
 
 export function partials(req: Request, res: Response, next: Function){
   res.render("partials/" + req.params.partial);
+};
+
+export function notfound(req: Request, res: Response, next: Function){
+  res.status(404).send("404 Not Found");
 };
