adc8696fa47bd3c3af2f5157c8f087aa078e2a0d
David Gee <git@allpowerfuldave.com>
2016-03-15 14:23:11 -0500

    Added funds component to routing

--- client/src/app/app.configuration.ts
+++ client/src/app/app.configuration.ts
@@ -1,12 +1,12 @@
-//import {RouteDefinition} from "angular2/router";
-
 import {DashboardComponent} from "./dashboard.component";
 import {ActivityComponent} from "./activity.component";
+import {FundsComponent} from "./funds.component";
 
 export var AppConfig = {
 
   routes: [
     {path: "/dashboard", name: "Dashboard", component: DashboardComponent, useAsDefault: true},
+    {path: "/funds", name: "Funds", component: FundsComponent},
     {path: "/activity", name: "Activity", component: ActivityComponent}
   //  {path: "/funds", name: "Funds", component: FundsComponent},
   //  {path: "/reporting", name: "Reporting, component: ReportingComponent}
