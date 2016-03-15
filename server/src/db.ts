1a4c4f13c3fe5a00563c660d73e4b38b1545b043
David Gee <git@allpowerfuldave.com>
2016-03-14 23:31:22 -0500

    Updated db to reflected renamed globals file

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,11 @@
 import * as sqlite3 from "sqlite3";
-import * as globals from "./app.globals";
+import {DATABASE} from "./globals";
 
 export function all(sql: string, params?: any[]): Promise<any> {
-  console.log(globals);
-  globals.DATABASE.all("select * from funds", (err, data) =>{ console.log(err); console.log(data); });
+
   return new Promise((resolve, reject) => {
 
-    globals.DATABASE.all(sql, params, (err, data) => {
+    DATABASE.all(sql, params, (err, data) => {
 
       if(null === err){
         resolve({
