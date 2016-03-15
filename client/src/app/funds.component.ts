2ad5dac08001a81bb805843e7aba50f485ffb704
David Gee <git@allpowerfuldave.com>
2016-03-15 14:17:59 -0500

    Added funds partial and component

---
+++ client/src/app/funds.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: "wcir-funds",
+  templateUrl: "/partials/funds"
+})
+export class FundsComponent {
+
+  public _funds = [
+    {symbol: "ANEFX", name: "American Funds The New Economy Fund®  Class A"}
+  ];
+}
+
+
+
