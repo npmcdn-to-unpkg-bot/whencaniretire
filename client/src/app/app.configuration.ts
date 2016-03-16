d932f58c45880b1a1c0676b2cce33c2598249816
David Gee <git@allpowerfuldave.com>
2016-03-16 09:42:48 -0500

    Client routing changes

--- client/src/app/app.configuration.ts
+++ client/src/app/app.configuration.ts
@@ -8,8 +8,6 @@ export var AppConfig = {
     {path: "/dashboard", name: "Dashboard", component: DashboardComponent, useAsDefault: true},
     {path: "/funds", name: "Funds", component: FundsComponent},
     {path: "/activity", name: "Activity", component: ActivityComponent}
-  //  {path: "/funds", name: "Funds", component: FundsComponent},
-  //  {path: "/reporting", name: "Reporting, component: ReportingComponent}
   ]
 
 };
