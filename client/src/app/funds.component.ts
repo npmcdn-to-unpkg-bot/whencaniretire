b1dc8f7150df0f3de71ce0d37b5d00625aba439c
David Gee <git@allpowerfuldave.com>
2016-03-20 22:52:41 -0500

    Funds service successfully using Observable API to get data

--- client/src/app/funds.component.ts
+++ client/src/app/funds.component.ts
@@ -1,7 +1,8 @@
-import {Component, Injector, provide} from "angular2/core";
+import {Component, Injector, provide, OnInit} from "angular2/core";
 import {RouteConfig} from "angular2/router";
 import {FundsService} from "./funds.service";
 import {Modal, ModalConfig, ModalDialogInstance, YesNoModal, YesNoModalContent, ICustomModal} from "angular2-modal";
+import {Observable} from "rxjs/Observable";
 
 interface Fund {
   symbol: string;
@@ -12,14 +13,20 @@ interface Fund {
   selector: "wcir-funds",
   templateUrl: "/partials/funds"
 })
-export class FundsComponent {
-
-  constructor(private _fundsService: FundsService, private _modal: Modal){
-    this._funds = _fundsService.funds;
-  }
+export class FundsComponent implements OnInit {
 
   public _dialog: Promise<ModalDialogInstance>;
   public _funds: Fund[];
+  private _fundsObserver: Observable<Fund[]>
+
+  constructor(private _fundsService: FundsService, private _modal: Modal){
+  }
+
+  public ngOnInit(){
+    this._fundsService.funds$.subscribe(updatedFunds => this._funds = updatedFunds);
+    this._fundsService.getAll();
+
+  }
 
   editFund(symbol: string): void {
     console.log(symbol);
--- client/src/app/funds.service.ts
+++ client/src/app/funds.service.ts
@@ -1,36 +1,58 @@
 import {Injectable} from "angular2/core";
 import {Http} from "angular2/http";
-import "rxjs/operator/add/map";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/share";
+import {Observable} from "rxjs/Observable";
+import {Observer} from "rxjs/Observer"
 
 interface Fund {
   symbol: string,
   name: string
 };
 
+interface Datastore {
+  funds: Fund[]
+};
+
 @Injectable()
 export class FundsService {
 
+  private _dataStore: Datastore;
+  private _fundsObserver: Observer<Fund[]>;
+  private _funds$: Observable<Fund[]>;
+
   constructor(private _http: Http){
+    this._funds$ = new Observable(observer => this._fundsObserver = observer).share();
+    this._dataStore = {
+      funds: []
+    };
   }
 
-  private _funds = [{symbol: "ANEFX", name: "American Funds The New Economy Fund® Class A"}];
-
-  public get funds(): any {
+  /*public get funds(): any {
 
     console.log("in funds getter");
-    return this.getAll();
+  }*/
+
+  public get funds$(): Observable<Fund[]> {
+
+    return this._funds$;
+
   }
 
 
 
-  public getAll(): any {
+  public getAll(): void {
 
-    return this._http.get("/api/funds").map(res => res.json());
-
-    /*this._http.get("/api/funds")
+    this._http
+      .get("/api/funds")
       .map(res => res.json())
-      .subscribe(funds => this._funds = funds);
-    */
+      .subscribe(data => {
+        this._dataStore.funds = data;
+        this._fundsObserver.next(this._dataStore.funds)
+      }, error => {
+        console.log("Could not load funds");
+      });
+
   }
 
 };
