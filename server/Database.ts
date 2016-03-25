d4a769df33bfaccdcaa693f35065ecace318b6dd
David Gee <git@allpowerfuldave.com>
2016-03-25 12:56:00 -0500

    Moved database model and related classes to Database file

--- server/Database.ts
+++ server/Database.ts
@@ -35,7 +35,7 @@ export class Database implements Database.DatabaseIntf {
   // Takes a SQL query and optional parameters and executes it
   // Returns a Promise which can be consumed with an async/await method
   // Returns all data found.
-  all(sql: string, params?: any): Promise<any> {
+  public all(sql: string, params?: any): Promise<any> {
     console.log("in all " + sql);
 
     return new Promise((resolve, reject) => {
@@ -59,3 +59,108 @@ export class Database implements Database.DatabaseIntf {
 }
 
 
+export enum SortDirection {
+  Ascending,
+  Descending,
+  None
+};
+
+export interface DatabaseFieldIntf {
+
+  name: string;
+  alias?: string;
+  mandatory?: boolean;
+  primaryKey?: boolean;
+  sort?: SortDirection;
+};
+
+export class DatabaseField implements DatabaseFieldIntf {
+
+  public name: string;
+  public alias: string;
+  public mandatory: boolean;
+  public primaryKey: boolean;
+  public sort: SortDirection;
+
+  constructor(options: DatabaseFieldIntf ){
+    if(options.name === undefined) throw new Error("Name is required");
+    else this.name = options.name;
+    if(options.alias !== undefined && options.alias !== "") this.alias = options.alias;
+    else this.alias = this.name;
+    if(options.mandatory !== undefined) this.mandatory = options.mandatory;
+    else this.mandatory = false;
+    if(options.primaryKey !== undefined) this.primaryKey = options.primaryKey;
+    else this.primaryKey = false;
+    if(options.sort !== undefined) this.sort = options.sort;
+    else this.sort = SortDirection.None;
+  }
+
+  public validate(value: any): boolean {
+    if(this.mandatory && !this.primaryKey){
+      if(value === undefined || value === null || (typeof value === "string" && value.length === 0)) return false;
+    }
+    return true;
+  }
+
+  public select(): string {
+    return this.name + " as " + this.alias;
+  }
+
+  public insert(): string {
+    return (this.primaryKey ? null : this.name);
+  }
+
+  public sortClause(): string {
+    switch(this.sort){
+      case SortDirection.None:
+        return null;
+      case SortDirection.Ascending:
+        return this.name + " ASC";
+      case SortDirection.Descending:
+        return this.name + " DESC";
+    }
+  }
+};
+
+
+export class DatabaseModel {
+
+  public fields: DatabaseField[];
+  private table: string;
+  private db: Database;
+
+  constructor(database: Database, table: string){
+
+    this.fields = [];
+    this.db = database;
+    this.table = table;
+
+  }
+
+  public addField(f: DatabaseField): void {
+    this.fields.push(f);
+  }
+
+  public select(where?: any): string {
+
+    let selectList = this.fields.map(f => f.select()).join(",");
+    let whereClause = (where.length > 0 ? (" WHERE " + where.map(w => w.field + " = $" + w.field).join(" AND ")) : "");
+    let sortList = this.fields.map(f => f.sortClause()).filter(f => (f !== null)).join(",");
+
+    return "SELECT " + selectList + " FROM " + this.table + whereClause + " ORDER BY " + sortList;
+
+  }
+
+  public async getAll(where?: any): Promise<any> {
+    if(where === undefined) where = {};
+    let whereValueList = {};
+    for(var prop in where){
+      whereValueList["$" + prop] = where[prop];
+    }
+    return await this.db.all(this.select(where), whereValueList);
+
+  };
+
+
+
+};
