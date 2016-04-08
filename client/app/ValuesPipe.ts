ad1dd377da3c6c479653960cb9eed82c3edd2b2e
David Gee <git@allpowerfuldave.com>
2016-04-07 19:05:33 -0500

    Cleaned up and simplified values pipe

--- client/app/ValuesPipe.ts
+++ client/app/ValuesPipe.ts
@@ -3,33 +3,14 @@ import {Pipe, PipeTransform} from 'angular2/core';
 @Pipe({name: 'values'})
 export class ValuesPipe implements PipeTransform {
 
-  transform(values: any, args?: any[]): Object[] {
-    let keys: string[] = Object.keys(values);
-    let data: any[] = [];
-    let keyName: string = "_id";
-    if(args.length > 0 && args[0].length > 0 && args[0] != null) keyName = args[0];
+  public transform(values: any, args?: any[]): Object[] {
 
-    /*let x = keys.map(key => {
-      console.log(value[key]);
-      value[key][keyName] = key;
-      return value;
-      //data.push(value[key])
+    if(values === undefined) return undefined;
+    if(values == null) return null;
+
+    return Object.keys(values).map(k => {
+      return values[k];
     });
-
-    console.log(x);
-
-    return x;
-
-    //return data;
-    */
-    return keys.map(k => {
-      return {
-        index: k,
-        value: values[k]
-      }
-    });
-
-
 
   }
 }
